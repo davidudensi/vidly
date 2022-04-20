1a2d7a8d6a10ae737bef76e701554a5fa572f890
David Udensi <davidudensi@gmail.com>
2022-04-20 11:58:38 +0100

    restructuring

--- routes/genres.js
+++ routes/genres.js
@@ -1,14 +1,6 @@
+const { Genre, validate } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
-
-const mongoose = require("mongoose");
-const Joi = require("joi");
-
-const genreSchema = mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Genre = mongoose.model("Genre", genreSchema);
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find();
@@ -23,7 +15,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const course = new Genre({
@@ -38,7 +30,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(
@@ -57,13 +49,5 @@ router.delete("/:id", async (req, res) => {
     return res.status(404).send("The genre with the given ID was not found.");
   res.send(genre);
 });
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
