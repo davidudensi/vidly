022297beda8dd40a4e6f9a185f0b00200fa22523
davidudensi <davidudensi@gmail.com>
2022-04-26 15:31:41 +0100

    hashing, jwt etc

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,26 @@
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered');
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send("User already registered");
 
-  try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    await user.save();
-    res.send(user);
-  } catch (ex) {
-    console.log(ex);
-    res.status(500).send("An error occured while adding new user");
-  }
+    try {
+        user = new User(_.pick(req.body, ["name", "email", "password"]));
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        await user.save();
+        res.send(_.pick(user, ["_id", "name", "email"]));
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).send("An error occured while adding new user");
+    }
 });
 
 module.exports = router;
